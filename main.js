50d827dd331478af85d78ee5815b00391c4ed661
Anjor-Srivastava <81854563+Anjor-Srivastava@users.noreply.github.com>
2022-04-17 10:48:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var prediction;
+var output;
 if (screen.width <= 565) {
     document.getElementById("instruct").style.fontSize = "30px";
     document.getElementById("btn_heading").style.letterSpacing = "5px";
@@ -23,13 +25,53 @@ function take_snapshot() {
 }
 
 console.log(ml5.version);
-var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wiVKqs4WP/model.json', model_loaded);
+var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DkItGIcqv/model.json', model_loaded);
 function model_loaded() {
     console.log("model loaded");
 }
 
-function speech(prediction) {
+function speech() {
     var synth = window.speechSynthesis;
-    var speech_data = new SpeechSynthesisUtterance(prediction);
+    var speech_data = new SpeechSynthesisUtterance(output);
     synth.speak(speech_data);
+}
+
+function check_results() {
+    var img = document.getElementById("img_snapshot");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if(error) {
+        console.error(error);
+        alert("Sorry, but an error occured");
+    }
+    else {
+        var emogi;
+        console.log(result);
+        prediction = result[0].label;
+        console.log(prediction);
+        if(prediction == "Amazing") {
+            output = "This is so amazing."
+            emogi = "&#128076;";
+        }
+        if(prediction == "Best") {
+            output = "All the best."
+            emogi = "&#128077;";
+        }
+        if(prediction == "Victory") {
+            output = "That was a marvellous vicotry."
+            emogi = "&#9996;";
+        }
+        if(prediction == "Hifi") {
+            output = "Hifi!!"
+            emogi = "&#128591;";
+        }
+        if(prediction == "Like") {
+            output = "Thanks!! I Like you too!"
+            emogi = "&#129311;";
+        }
+        document.getElementById("result").innerHTML = emogi + "<br>" + output;
+        speech();
+    }
 }
